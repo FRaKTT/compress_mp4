0eee8aaeafe9896fff1faeae1526c5d7ae6e57e4
Fedor <fraktt@yandex.ru>
2020-06-14 14:35:53 +0300

    3rd commit

--- compress.py
+++ compress.py
@@ -3,21 +3,33 @@ import sys
 import subprocess
 from shutil import copyfile
 
-
+# exiftool flags
 EXIF_DATES = ['-createDate', '-mediaCreateDate', '-trackCreateDate', 
               '-modifyDate', '-mediaModifyDate', '-trackModifyDate']
 
 
+def run_command(cmd):
+    """Run command using subprocess module"""
+    print(' '.join(cmd))
+    return_code = subprocess.run(cmd)
+    return return_code
+
+
 def transcode(src, dst):
-    """exec command: ffmpeg -i src_file -c:v libx264 -c:a copy -y dst_file"""
-    cmd = ['ffmpeg', '-i', src, '-c:v', 'libx264', '-c:a', 'copy', '-y', dst]  # form command
-    print(cmd)
-    return_code = subprocess.call(cmd)  # run command
-    print(return_code)
+    """Exec ffmpeg command"""
+    # ffmpeg -i src_file -c:v libx264 -c:a copy -y dst_file
+    cmd = [
+        'ffmpeg', 
+        '-i', src, 
+        '-c:v', 'libx264', # libx264 for video
+        '-c:a', 'copy', # copy audio
+        '-y', # overwrite output file without asking 
+        dst]
+    run_command(cmd)
 
 
 def names(src_files):
-    """selecting mp4 files and forming names with original marks"""
+    """Select mp4 files and form names with 'original' marks"""
     src_orig_pairs = []
     orig_mark = '_original'
     for src in src_files:
@@ -32,17 +44,13 @@ def names(src_files):
 
 
 def copy_metadata_dates(src, dst):
-    cmd = ['exiftool', '-tagsFromFile', src] + EXIF_DATES + [dst]
-    print(cmd)
-    return_code = subprocess.call(cmd)
+    cmd = ['exiftool', '-tagsFromFile', src, '-overwrite_original'] + EXIF_DATES + [dst]
+    run_command(cmd)
 
 
-def get_metadata(tags, f):
-    # cmd = ['exiftool'] + EXIF_DATES + [f]
-    # cmd = ['exiftool', '-avgbitrate', target]
-    cmd = ['exiftool'] + tags + [f]
-    print(cmd)
-    return_code = subprocess.call(cmd)
+def get_metadata(filename, *tags):
+    cmd = ['exiftool'] + list(tags) + [filename]
+    run_command(cmd)
 
 # strange changes after transcoding:
 # Media Time Scale: 90к -> 48k
@@ -50,18 +58,20 @@ def get_metadata(tags, f):
 # Video Frame Rate: 29.991 -> 30
 
 if __name__ == '__main__':
-    if len(sys.argv) < 2:
+    try:
+        arguments = sys.argv[1:]  # skip script name (sys.argv[0])
+    except:
         print('need files in arguments:')
-        print('> python3 transcode.py target.mp4')
+        print('> python3 compress.py target.mp4')
         sys.exit()
-    arguments = sys.argv[1:]  # skip script name
-    names_pairs = names(arguments)
-    for target, orig in names_pairs:
+
+    for target, orig in names(arguments):
         copyfile(target, orig)  # copy original file
         transcode(orig, target)  # compress file
-        get_metadata(EXIF_DATES + ['-avgBitrate'], orig)
-        get_metadata(EXIF_DATES + ['-avgBitrate'], target)
+        print()
+        get_metadata(orig, *EXIF_DATES, '-avgBitrate')
+        get_metadata(target, *EXIF_DATES, '-avgBitrate')
         copy_metadata_dates(orig, target)  # copy dates
-        get_metadata(EXIF_DATES + ['-avgBitrate'], target)
+        get_metadata(target, *EXIF_DATES, '-avgBitrate')
 
 
